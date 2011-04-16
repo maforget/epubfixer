59c6c3d229e5999dce6f5a8980702aee1314db2d
ePubFixer@gmail.com <ePubFixer@gmail.com@615ad4b2-1684-f78e-fe82-360227150ece>
2011-04-16 21:09:25 +0000

    Enabled Navigation in the preview window

--- ePubFixer/Forms/frmPreview.Designer.cs
+++ ePubFixer/Forms/frmPreview.Designer.cs
@@ -36,7 +36,6 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.AllowNavigation = false;
             this.webBrowser1.AllowWebBrowserDrop = false;
             this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
