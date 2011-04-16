96f2e74dc2f9606d90c28ac86dde02039df9f8de
ePubFixer@gmail.com <ePubFixer@gmail.com@615ad4b2-1684-f78e-fe82-360227150ece>
2011-04-16 16:25:21 +0000

    Completely Redone Way that the Mass Renamer tool works to fix bugs and make it more modular

--- trunk/ePubFixer/Forms/frmMassRename.cs
+++ trunk/ePubFixer/Forms/frmMassRename.cs
@@ -82,99 +82,172 @@ namespace ePubFixer
         #endregion
 
         #region Rename
-        private void ConvertToText(string text)
+        //private void ConvertToText(string text)
+        //{
+        //    string Prefix = String.Empty;
+        //    string Number = String.Empty;
+        //    string Sufix = String.Empty;
+        //    string Prefix2 = String.Empty;
+        //    string Number2 = String.Empty;
+        //    string Sufix2 = String.Empty;
+
+        //    Regex regexObj = new Regex(@"(?<Prefix>[^\d]*)(?<Number>\d*)(?<Sufix>.*$)");
+        //    Prefix = regexObj.Match(text).Groups["Prefix"].Value;
+        //    Number = regexObj.Match(text).Groups["Number"].Value;
+        //    Sufix = regexObj.Match(text).Groups["Sufix"].Value;
+
+        //    double num = 0;
+        //    double.TryParse(Number, out num);
+
+        //    if (num <= 0)
+        //        Number = String.Empty;
+
+        //    bool FirstNumberFound = false;
+        //    for (int i = 0; i < InputText.Count; i++)
+        //    {
+
+        //        string TextToKeep = Regex.Replace(text, "%T", InputText[i], RegexOptions.IgnoreCase);
+        //        if (Prefix.ToUpper().Contains("%T") || Sufix.ToUpper().Contains("%T"))
+        //        {
+        //            Prefix2 = regexObj.Match(TextToKeep).Groups["Prefix"].Value;
+        //            Sufix2 = regexObj.Match(TextToKeep).Groups["Sufix"].Value;
+        //            Number2 = regexObj.Match(TextToKeep).Groups["Number"].Value;
+
+        //            if (!string.IsNullOrEmpty(Number2) && !FirstNumberFound)
+        //            {
+        //                if (double.TryParse(Number2, out num))
+        //                    FirstNumberFound = true;
+
+        //            } else if (string.IsNullOrEmpty(Number2) && num > 0 && string.IsNullOrEmpty(Number))
+        //            {
+        //                num = 0;
+        //                FirstNumberFound = false;
+        //            }
+        //        } else
+        //        {
+        //            Prefix2 = Prefix;
+        //            Sufix2 = Sufix;
+        //        }
+
+        //        if (num > 0)
+        //        {
+        //            if (cbConvertToWords.Checked || cbRoman.Checked)
+        //            {
+        //                Number = cbRoman.Checked ? NumberToWordsConverter.NumberToRoman(num)
+        //            : NumberToWordsConverter.NumberToWords(num);
+        //                //Number = cbUpperCase.Checked ? Number.ToUpper() : Number;  
+        //            } else
+        //            {
+        //                Number = Number2 != string.Empty ? Number2 : num.ToString();
+        //            }
+        //        }
+
+        //        _text.Add(Prefix2 + Number + Sufix2);
+        //        if (num > 0)
+        //        {
+        //            num++;
+        //        }
+
+        //        Number = string.Empty;
+        //    }
+
+        //    ConvertCase(_text);
+        //}
+
+        private void ConvertToText(string InText)
         {
-            string Prefix = String.Empty;
-            string Number = String.Empty;
-            string Sufix = String.Empty;
-            string Prefix2 = String.Empty;
-            string Number2 = String.Empty;
-            string Sufix2 = String.Empty;
+            List<string> TextList = ReplaceSpecialTag(InText);
+            List<ParsedText> Converted = ConvertToWords(TextList);
+            ConvertCase(Converted);
 
-            Regex regexObj = new Regex(@"(?<Prefix>[^\d]*)(?<Number>\d*)(?<Sufix>.*$)");
-            Prefix = regexObj.Match(text).Groups["Prefix"].Value;
-            Number = regexObj.Match(text).Groups["Number"].Value;
-            Sufix = regexObj.Match(text).Groups["Sufix"].Value;
-
-            double num = 0;
-            double.TryParse(Number, out num);
-
-            if (num <= 0)
-                Number = String.Empty;
-
-            bool FirstNumberFound = false;
-            for (int i = 0; i < InputText.Count; i++)
-            {
-
-                string TextToKeep = Regex.Replace(text, "%T", InputText[i], RegexOptions.IgnoreCase);
-                if (Prefix.ToUpper().Contains("%T") || Sufix.ToUpper().Contains("%T"))
-                {
-                    Prefix2 = regexObj.Match(TextToKeep).Groups["Prefix"].Value;
-                    Number2 = regexObj.Match(TextToKeep).Groups["Number"].Value;
-                    Sufix2 = regexObj.Match(TextToKeep).Groups["Sufix"].Value;
-
-                    if (!string.IsNullOrEmpty(Number2) && !FirstNumberFound)
-                    {
-                        double.TryParse(Number2, out num);
-                        FirstNumberFound = true;
-                    } else if (string.IsNullOrEmpty(Number2) && num > 0)
-                    {
-                        num = 0;
-                        FirstNumberFound = false;
-                    }
-                } else
-                {
-                    Prefix2 = Prefix;
-                    Sufix2 = Sufix;
-                }
-
-                if (num > 0)
-                {
-                    if (cbConvertToWords.Checked || cbRoman.Checked)
-                    {
-                        Number = cbRoman.Checked ? NumberToWordsConverter.NumberToRoman(num)
-                    : NumberToWordsConverter.NumberToWords(num);
-                        //Number = cbUpperCase.Checked ? Number.ToUpper() : Number;  
-                    } else
-                    {
-                        if (Number != string.Empty)
-                        {
-                            Number = num.ToString();
-                        } else if (Number2 != string.Empty)
-                        {
-                            Number = Number2;
-                        }
-                    }
-                }
-
-                _text.Add(Prefix2 + Number + Sufix2);
-                if (!string.IsNullOrEmpty(Number))
-                {
-                    num++;
-                }
-
-                Number = string.Empty;
-            }
-
-            _text = ConvertCase(_text);
         }
 
-        private List<string> ConvertCase(List<string> text)
+        private List<string> ReplaceSpecialTag(string InText)
         {
-            for (int i = 0; i < text.Count; i++)
-            {
-                string item = text[i];
-                CultureInfo cultureInfo = CultureInfo.InvariantCulture;
-                TextInfo textInfo = cultureInfo.TextInfo;
+            List<string> text = new List<string>();
 
-                text[i] = cbUpperCase.Checked ? textInfo.ToUpper(item) :
-                    cbTitleCase.Checked ? textInfo.ToTitleCase(item.ToLower()) : item;
+            for (int i = 0; i < InputText.Count; i++)
+            {
+                text.Add(Regex.Replace(InText, "%T", InputText[i], RegexOptions.IgnoreCase));
+                //All lines should be the same SOme TExt - 1 
+                //for example if I had put %t - 1 where Sometext was the original text
             }
 
             return text;
         }
+
+        private List<ParsedText> ConvertToWords(List<string> TextToParse)
+        {
+            List<ParsedText> Parsed = ParseText(TextToParse);
+
+            for (int i = 0; i < Parsed.Count; i++)
+            {
+                ParsedText p = Parsed[i];
+
+                if (p.num > 0)
+                {
+                    if (i > 0 && p.OrigiNum == Parsed[i - 1].OrigiNum)
+                    {
+                        //The original number are all the same we should increment them
+                        p.num = Parsed[i - 1].num + 1;
+                        p.Number = p.num.ToString();
+                    }
+
+                    if (cbConvertToWords.Checked || cbRoman.Checked)
+                    {
+                        p.Number = cbRoman.Checked ? NumberToWordsConverter.NumberToRoman(p.num)
+                    : NumberToWordsConverter.NumberToWords(p.num);
+                        //Number = cbUpperCase.Checked ? Number.ToUpper() : Number;  
+                    } 
+                }
+
+                p.FinalText = p.Prefix + p.Number + p.Sufix;
+            }
+
+            return Parsed;
+        }
+
+        private List<ParsedText> ParseText(List<string> TextToParse)
+        {
+            List<ParsedText> Parsed = new List<ParsedText>();
+
+            for (int i = 0; i < TextToParse.Count; i++)
+            {
+                string InText = TextToParse[i];
+                ParsedText p = new ParsedText();
+
+                Regex regexObj = new Regex(@"(?<Prefix>[^\d]*)(?<Number>\d*)(?<Sufix>.*$)");
+                p.Prefix = regexObj.Match(InText).Groups["Prefix"].Value;
+                p.Number = regexObj.Match(InText).Groups["Number"].Value;
+                p.Sufix = regexObj.Match(InText).Groups["Sufix"].Value;
+
+                int num = 0;
+                int.TryParse(p.Number, out num);
+
+                p.num = num;
+                p.OrigiNum = num;
+
+                Parsed.Add(p);
+            }
+
+            return Parsed;
+        }
+
+        private void ConvertCase(List<ParsedText> text)
+        {
+            for (int i = 0; i < text.Count; i++)
+            {
+                string item = text[i].FinalText;
+                CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+                TextInfo textInfo = cultureInfo.TextInfo;
+
+                _text.Add(cbUpperCase.Checked ? textInfo.ToUpper(item) :
+                    cbTitleCase.Checked ? textInfo.ToTitleCase(item.ToLower()) : item);
+            }
+        }
         #endregion
 
+        #region Show Help
         void ShowHelp()
         {
             string message =
@@ -201,8 +274,21 @@ and Number To Words and it will change it to Chapter One.";
         {
             ShowHelp();
         }
+        #endregion
+
 
 
         //TODO remove text tool
+    }
+
+
+    class ParsedText
+    {
+        public string Prefix { get; set; }
+        public string Number { get; set; }
+        public string Sufix { get; set; }
+        public int num { get; set; }
+        public int OrigiNum { get; set; }
+        public string FinalText { get; set; }
     }
 }
