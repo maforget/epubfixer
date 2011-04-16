b6bbd455864d3a87bef25e08a824230a67850dd5
ePubFixer@gmail.com <ePubFixer@gmail.com@615ad4b2-1684-f78e-fe82-360227150ece>
2011-04-16 21:06:24 +0000

    Added a Download button in the Add window, that will search the site Kobobooks for the TOC and add the entries found to the detected text menu; Added a Select Next Text (Incremental) in the Add window, to help selecting the chapters when using the Download button.

---
+++ ePubFixer/Utils/InputBox.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+class InputBox
+{
+    public string Value { get; set; }
+    public DialogResult ShowDialog(string title, string promptText)
+    {
+        Form form = new Form();
+        Label label = new Label();
+        TextBox textBox = new TextBox();
+        Button buttonOk = new Button();
+        Button buttonCancel = new Button();
+
+        form.Text = title;
+        label.Text = promptText;
+
+        buttonOk.Text = "OK";
+        buttonCancel.Text = "Cancel";
+        buttonOk.DialogResult = DialogResult.OK;
+        buttonCancel.DialogResult = DialogResult.Cancel;
+
+        label.SetBounds(9, 20, 372, 13);
+        textBox.SetBounds(12, 36, 372, 20);
+        buttonOk.SetBounds(228, 72, 75, 23);
+        buttonCancel.SetBounds(309, 72, 75, 23);
+
+        label.AutoSize = true;
+        textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+        buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+        form.ClientSize = new Size(396, 107);
+        form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+        form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+        form.FormBorderStyle = FormBorderStyle.FixedDialog;
+        form.StartPosition = FormStartPosition.CenterScreen;
+        form.MinimizeBox = false;
+        form.MaximizeBox = false;
+        form.AcceptButton = buttonOk;
+        form.CancelButton = buttonCancel;
+
+        DialogResult dialogResult = form.ShowDialog();
+        Value = textBox.Text;
+        return dialogResult;
+    }
+}
