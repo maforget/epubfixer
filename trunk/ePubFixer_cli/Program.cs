3c33c1860f9d647e91cf5d4878143b66f8efe939
ePubFixer@gmail.com <ePubFixer@gmail.com@615ad4b2-1684-f78e-fe82-360227150ece>
2012-07-28 03:17:32 +0000

    CLI Version

---
+++ trunk/ePubFixer_cli/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+using System.ComponentModel;
+using System.Reflection;
+using System.IO;
+
+namespace ePubFixer_cli
+{
+    class Program
+    {
+        private sealed class Options : CommandLineOptionsBase
+        {
+            #region Standard Option Attribute
+            [Option("f", "file", Required = true, HelpText = "ePub file to process")]
+            public string InputFile { get; set; }
+
+            [Option("t", "toc", HelpText = "Create a Table of Content")]
+            public bool CreateTOC { get; set; }
+
+            [Option("h", "html", DefaultValue = false, HelpText = "Add a HTML Table of Content to the file (Requires -t)")]
+            public bool CreateHtmlTOC { get; set; }
+
+            [Option("c", "chapter", DefaultValue = false, HelpText = "Retrieve title of Chapters automatically (Requires -t, will use the filename if not set)")]
+            public bool RetrieveChapterText { get; set; }
+            #endregion
+
+            #region Help Screen
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+            #endregion
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = ePubFixer.Utils.GetTitle();
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(Resolver);
+            var options = new Options();
+            var parser = new CommandLineParser(new CommandLineParserSettings(false, true, Console.Error));
+            if (!parser.ParseArguments(args, options))
+                Environment.Exit(1);
+
+            DoCoreTask(options);
+
+            Environment.Exit(0);
+        }
+
+        private static void DoCoreTask(Options options)
+        {
+            Console.WriteLine(Environment.NewLine);
+
+            if (!File.Exists(options.InputFile))
+            {
+                Console.WriteLine("Error : File Not Found, Exiting");
+                Environment.Exit(1);
+            }
+
+            ePubFixer.Variables.Filename = options.InputFile;
+            ePubFixer.Variables.CreateHtmlTOC = options.CreateHtmlTOC;
+            ePubFixer.Variables.TextInChapters = options.RetrieveChapterText;
+            int optCount = 0;
+
+            if (options.CreateTOC)
+            {
+                optCount++;
+                TOC toc = new TOC();
+                toc.UpdateFile();
+                Console.WriteLine("TOC has been created");
+            }
+
+            if (optCount == 0)
+                Console.WriteLine("No Options Selected, see help\n\n\n" + options.GetUsage());
+        }
+
+        static System.Reflection.Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            //This handler is called only when the common language runtime tries to bind to the assembly and fails.
+
+            //Load required DLL form a embeded Ressources
+            Assembly a1 = Assembly.GetExecutingAssembly(); // Get Executing Assembly 
+
+            //Find the Name of the missing Dll to Load
+            string Name = a1.GetName().Name + ".DLL." + args.Name.Split(',')[0];
+
+            //Get a List of Embeded Ressources
+            string[] str = a1.GetManifestResourceNames();
+            byte[] block = null;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].StartsWith(Name))//Does not check for the Extension, SO multiple extension are a not OK. Or if the File Name does not match the Namespace
+                {
+                    Stream s = a1.GetManifestResourceStream(str[i]);
+                    block = new byte[s.Length];
+                    s.Read(block, 0, block.Length);
+                    break;
+                }
+            }
+
+            Assembly a2 = Assembly.Load(block);
+            return a2;
+        }
+    }
+}
---
+++ trunk/ePubFixer_cli/TOC.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using ePubFixer;
+
+namespace ePubFixer_cli
+{
+    class TOC : TocDocument
+    {
+        private int playOrder;
+
+        public TOC()
+            : base()
+        {
+            Utils.NewFilename();
+        }
+
+        internal override void UpdateFile()
+        {
+            playOrder = 0;
+            fileExtractStream = base.GetStreamOPF(Variables.NCXFile);
+            //Get the Existing Table of COntent
+            XElement xml = GetXmlElement("navMap");
+
+            AutoCreateNewTOC();
+        }
+
+        private void AutoCreateNewTOC()
+        {
+            var toc = LoadFiles();
+            Save(toc);
+        }
+
+        private XElement LoadFiles()
+        {
+            //Get Files LIst
+            OpfDocument OpfDoc = new OpfDocument();
+            List<string> htmlFileLIst = OpfDoc.GetFilesList("html");
+            List<string> t = htmlFileLIst;
+
+            //Load text from Chapter
+            MyHtmlDocument htmlDoc = new MyHtmlDocument();
+            Dictionary<string, DetectedHeaders> DetectText = htmlDoc.FindHeaderTextInFile(t);
+
+            //Create NavDetails ENtry for Each FIles
+            List<NavDetails> FilesToAdd = new List<NavDetails>();
+            foreach (var item in DetectText)
+            {
+                string Text = String.Empty;
+                Text = Variables.TextInChapters == true ? item.Value.Result.Count == 0 ? item.Key : item.Value.Result[0] : item.Key;
+                FilesToAdd.Add(new NavDetails(Utils.GetId(item.Key), item.Key, Text));
+            }
+
+            List<XElement> list = new List<XElement>();
+
+            //Converts the navdetails to xml
+            foreach (var item in FilesToAdd)
+            {
+                XElement navPoint = ConvertToXElement(item);
+                if (navPoint != null)
+                {
+                    list.Add(navPoint);
+                }
+            }
+
+            XElement navMap = new XElement(ns + "navMap", list);
+
+            //return the new xml file
+            return navMap;
+        }
+
+        private XElement ConvertToXElement(NavDetails item)
+        {
+            playOrder++;
+
+            XElement el =
+            new XElement(ns + "navPoint",
+            new XAttribute("id", item.navPointid),
+            new XAttribute("playOrder", playOrder),
+            new XElement(ns + "navLabel",
+            new XElement(ns + "text", item.Text)),
+            new XElement(ns + "content",
+            new XAttribute("src", (item.ContentSrc))));
+
+            return el;
+        }
+
+        private void Save(XElement xml)
+        {
+            NewNavMap = xml;
+
+            if (NewNavMap == null)
+                return;
+
+            ReplaceNavMap(NewNavMap);
+            WriteXML();
+
+            base.UpdateZip(fileOutStream);
+
+            if (Variables.CreateHtmlTOC == true)
+            {
+                HtmlTOCDocument doc = new HtmlTOCDocument(NewNavMap);
+                doc.UpdateFile();
+            }
+        }
+    }
+}
